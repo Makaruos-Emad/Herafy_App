366c9ff0b7bd31f8128fd9b8580253d733a87dfc
Makaruos Emad <makaruosemad235@gmail.com>
2026-03-10 18:39:00 +0200

    onboarding screen done

--- lib/features/onboarding/ui/widgets/onboarding_screen_body.dart
+++ lib/features/onboarding/ui/widgets/onboarding_screen_body.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:herafy/core/services/shared_preferences_singleton.dart';
+import 'package:herafy/core/utils/app_constants.dart';
+import 'package:herafy/core/widgets/custom_button.dart';
+import 'package:herafy/core/widgets/dots_indicator.dart';
+import 'package:herafy/features/onboarding/ui/widgets/onboarding_page_view.dart';
 import 'package:herafy/features/onboarding/ui/widgets/skip_button.dart';
 
-class OnboardingScreenBody extends StatelessWidget {
+class OnboardingScreenBody extends StatefulWidget {
   const OnboardingScreenBody({super.key});
+
+  @override
+  State<OnboardingScreenBody> createState() => _OnboardingScreenBodyState();
+}
+
+class _OnboardingScreenBodyState extends State<OnboardingScreenBody> {
+  late PageController pageController;
+
+  var currentPage = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+
+    pageController.addListener(() {
+      currentPage = pageController.page!.round();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [SkipButton()],
+      children: [
+        SkipButton(),
+        Expanded(child: OnboardingPageView(pageController: pageController)),
+        Column(
+          children: [
+            DotsIndicator(
+              currentPage: currentPage,
+              length: 3,
+              pageController: pageController,
+            ),
+            const SizedBox(height: 16),
+            CustomButton(
+              onPressed: () {
+                if (currentPage == 2) {
+                  Prefs.setBool(kIsOnboardingSeen, true);
+                  Navigator.pushReplacementNamed(context, "/loginNumberScreen");
+                } else {
+                  pageController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.linear,
+                  );
+                }
+              },
+              text: "التالي",
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
