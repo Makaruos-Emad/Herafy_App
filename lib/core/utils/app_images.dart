192a5086697e95023f3557c40ae78b76baae0f7b
Makaruos Emad <makaruosemad235@gmail.com>
2026-03-12 12:13:51 +0200

    add bottom navigatoin bar icons

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -2,6 +2,38 @@
 class Assets {
   Assets._();
   
+  /// Assets for imagesBottomNavigatoinBarIconsChat
+  /// assets/images/bottom_navigatoin_bar_icons/chat.svg
+  static const String imagesBottomNavigatoinBarIconsChat = "assets/images/bottom_navigatoin_bar_icons/chat.svg";
+
+  /// Assets for imagesBottomNavigatoinBarIconsChatBlue
+  /// assets/images/bottom_navigatoin_bar_icons/chat_blue.svg
+  static const String imagesBottomNavigatoinBarIconsChatBlue = "assets/images/bottom_navigatoin_bar_icons/chat_blue.svg";
+
+  /// Assets for imagesBottomNavigatoinBarIconsHome
+  /// assets/images/bottom_navigatoin_bar_icons/home.svg
+  static const String imagesBottomNavigatoinBarIconsHome = "assets/images/bottom_navigatoin_bar_icons/home.svg";
+
+  /// Assets for imagesBottomNavigatoinBarIconsHomeBlue
+  /// assets/images/bottom_navigatoin_bar_icons/home_blue.svg
+  static const String imagesBottomNavigatoinBarIconsHomeBlue = "assets/images/bottom_navigatoin_bar_icons/home_blue.svg";
+
+  /// Assets for imagesBottomNavigatoinBarIconsList
+  /// assets/images/bottom_navigatoin_bar_icons/list.svg
+  static const String imagesBottomNavigatoinBarIconsList = "assets/images/bottom_navigatoin_bar_icons/list.svg";
+
+  /// Assets for imagesBottomNavigatoinBarIconsListBlue
+  /// assets/images/bottom_navigatoin_bar_icons/list_blue.svg
+  static const String imagesBottomNavigatoinBarIconsListBlue = "assets/images/bottom_navigatoin_bar_icons/list_blue.svg";
+
+  /// Assets for imagesBottomNavigatoinBarIconsUser
+  /// assets/images/bottom_navigatoin_bar_icons/user.svg
+  static const String imagesBottomNavigatoinBarIconsUser = "assets/images/bottom_navigatoin_bar_icons/user.svg";
+
+  /// Assets for imagesBottomNavigatoinBarIconsUserBlue
+  /// assets/images/bottom_navigatoin_bar_icons/user_blue.svg
+  static const String imagesBottomNavigatoinBarIconsUserBlue = "assets/images/bottom_navigatoin_bar_icons/user_blue.svg";
+
   /// Assets for imagesLoginMobileNumber
   /// assets/images/loginMobileNumber.svg
   static const String imagesLoginMobileNumber = "assets/images/loginMobileNumber.svg";
