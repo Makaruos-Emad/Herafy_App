ae31ff6ee889aa11f1a97c972cc414664c1bd57c
Makaruos Emad <makaruosemad235@gmail.com>
2026-03-17 20:14:44 +0200

    add all component to home client screen

--- lib/features/home/ui/widget/home_client_screen_body.dart
+++ lib/features/home/ui/widget/home_client_screen_body.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:herafy/features/home/ui/widget/home_app_bar.dart';
+import 'package:herafy/features/home/ui/widget/most_requested_services.dart';
+import 'package:herafy/features/home/ui/widget/promo_slider.dart';
+import 'package:herafy/features/home/ui/widget/search_and_filter.dart';
+import 'package:herafy/features/home/ui/widget/services_section.dart';
 
 class HomeClientScreenBody extends StatelessWidget {
   const HomeClientScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [HomeAppBar()]);
+    return Column(
+      children: [
+        HomeAppBar(),
+        const SizedBox(height: 16),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SearchAndFilter(),
+                const SizedBox(height: 16),
+                PromoSlider(),
+                const SizedBox(height: 16),
+                ServicesSection(),
+                const SizedBox(height: 16),
+                MostRequestedServices(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
