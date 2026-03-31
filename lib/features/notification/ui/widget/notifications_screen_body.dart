7772473e0934da3ce096f24f03688a695c042b49
Makaruos Emad <makaruosemad235@gmail.com>
2026-03-31 10:43:39 +0200

    add EmptyStateWidget for notifications

--- lib/features/notification/ui/widget/notifications_screen_body.dart
+++ lib/features/notification/ui/widget/notifications_screen_body.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:herafy/core/widgets/custom_app_bar.dart';
+import 'package:herafy/features/notification/ui/widget/empty_state_widget.dart';
 
 class NotificationsScreenBody extends StatelessWidget {
   const NotificationsScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        CustomAppBar(
+          title: "الإشعارات",
+          onpress: () {
+            Navigator.pop(context);
+          },
+        ),
+        Expanded(child: Container(child: EmptyStateWidget())),
+      ],
+    );
   }
 }
