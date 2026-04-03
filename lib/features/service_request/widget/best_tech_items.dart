d20f03f32ad8d1fa52a9e1da7143e846b978cf05
muller <mollar.ashraf23@.com>
2026-04-04 00:32:36 +0200

    update and forgetes commit

--- lib/features/service_request/widget/best_tech_items.dart
+++ lib/features/service_request/widget/best_tech_items.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:herafy/core/routing/routes.dart';
 import 'package:herafy/core/theme/app_colors.dart';
 import 'package:herafy/core/theme/app_text_styles.dart';
 import 'package:herafy/core/utils/app_constants.dart';
@@ -91,7 +92,9 @@ class BestTechItems extends StatelessWidget {
             SizedBox(
               width: 350,
               child: CustomButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushNamed(context, Routes.technicialSelectedProfileScreen);
+                },
                 textStyle: AppTextStyles.regular16PrimaryColor,
                 side: const BorderSide(color: AppColors.primaryColor),
                 backgroundColor: Colors.white,
