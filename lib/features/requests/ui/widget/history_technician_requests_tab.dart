2e68ba8f6769cb3a07581e4446a2a9684838522e
Makaruos Emad <makaruosemad235@gmail.com>
2026-04-20 14:51:20 +0200

    Add HistoryTechnicianRequestsItem and HistoryTechnicianRequestsTab widgets

---
+++ lib/features/requests/ui/widget/history_technician_requests_tab.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:herafy/features/requests/model/requests_model.dart';
+import 'package:herafy/features/requests/ui/widget/history_technician_requests_item.dart';
+
+class HistoryTechnicianRequestsTab extends StatelessWidget {
+  const HistoryTechnicianRequestsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<RequestsModel> historyTechnicianRequests = [
+      RequestsModel(
+        id: "# 2543",
+        title: "صيانة مروحه",
+        icon: Icons.design_services,
+        status: "مكتمل",
+        time: DateTime.now(),
+        price: 150,
+      ),
+      RequestsModel(
+        id: "# 2544",
+        title: "صيانة باب",
+        icon: Icons.web,
+        status: "مكتمل",
+        time: DateTime.now().subtract(Duration(days: 2)),
+        price: 500,
+      ),
+    ];
+    return ListView.builder(
+      itemCount: historyTechnicianRequests.length,
+      itemBuilder: (context, index) {
+        return HistoryTechnicianRequestsItem(
+          request: historyTechnicianRequests[index],
+        );
+      },
+    );
+  }
+}
