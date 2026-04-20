3ccd76f81b7b3a57ac66b32140402ba19aa9a3e6
Makaruos Emad <makaruosemad235@gmail.com>
2026-04-20 10:56:03 +0200

    Implement RequestsTechnicianScreenBody

--- lib/features/requests/ui/requests_technician_screen_body.dart
+++ lib/features/requests/ui/requests_technician_screen_body.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:herafy/core/theme/app_text_styles.dart';
+import 'package:herafy/features/requests/ui/widget/request_tabs.dart';
 
 class RequestsTechnicianScreenBody extends StatelessWidget {
   const RequestsTechnicianScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Center(
+                child: Text("طلباتي", style: AppTextStyles.semiBold20Black),
+              ),
+              const SizedBox(height: 24),
+              const RequestTabs(
+                tabs: ["طلبات جديدة", "مهام قادمة", "سجل العمل"],
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
